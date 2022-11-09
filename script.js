35d093cc0fa0b60300f5a3302b1b5bfd68445389
Koporson <108631492+K1337xz@users.noreply.github.com>
2022-11-09 23:27:02 +0100

    Update
    update

--- script.js
+++ script.js
@@ -12,3 +12,36 @@ closeMobile.addEventListener("click", () => {
 	navItem.style.width = ``;
 	openMobile.style.display = `block`;
 });
+
+//pick destination
+async function loadData() {
+	const response = await fetch("data.json");
+	return response.json();
+}
+
+const destinationNav = document.querySelectorAll(".destinationBar li");
+
+for (let i = 0; i < destinationNav.length; i++) {
+	destinationNav[i].addEventListener("click", async () => {
+		try {
+			const destinatioName = document.querySelector(".nameofPlanet");
+			const destinationDescribe =
+				document.querySelector(".descriebPlanet");
+			const distance = document.querySelector(".distanceInner");
+			const timeLeft = document.querySelector(".timeInner");
+			const destinationImage =
+				document.querySelector(".activeDestination");
+
+			const data = await loadData();
+			const destination = data.destinations;
+			if (destinationNav[i].firstChild.textContent === `Mars`) {
+				const result = destination.filter((x) => x.name === "Moon");
+				console.log(result.name);
+			}
+		} catch (e) {
+			console.log("errror");
+			console.log(e);
+		}
+	});
+}
+console.log(destinationNav);
