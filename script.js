7d5c70f6e17c5b1fa63500a0d58d8e45f9edc730
Koporson <108631492+K1337xz@users.noreply.github.com>
2022-11-11 19:54:28 +0100

    End script

--- script.js
+++ script.js
@@ -13,17 +13,19 @@ closeMobile.addEventListener("click", () => {
 	openMobile.style.display = `block`;
 });
 
-//pick destination
+//load data fromjson file
 async function loadData() {
 	const response = await fetch("data.json");
 	return response.json();
 }
+//pick destination
 
 const destinationNav = document.querySelectorAll(".destinationBar li");
 
 for (let i = 0; i < destinationNav.length; i++) {
 	destinationNav[i].addEventListener("click", async () => {
 		try {
+			const data = await loadData();
 			const destinatioName = document.querySelector(".nameofPlanet");
 			const destinationDescribe =
 				document.querySelector(".descriebPlanet");
@@ -38,7 +40,6 @@ for (let i = 0; i < destinationNav.length; i++) {
 				}
 			}
 			destinationNav[i].classList.add("active");
-			const data = await loadData();
 			const destination = data.destinations;
 			let nameofClick = destinationNav[i].firstChild.textContent;
 
@@ -60,3 +61,38 @@ for (let i = 0; i < destinationNav.length; i++) {
 	});
 }
 //crew meber pick
+const crewNav = document.querySelectorAll(".dotsNav li");
+
+for (let z = 0; z < crewNav.length; z++) {
+	crewNav[z].addEventListener("click", async () => {
+		try {
+			const data = await loadData();
+			const role = document.querySelector(".role p");
+			const crewName = document.querySelector(".crewBio h1");
+			const crewDescription = document.querySelector(".crewBio p");
+			const crewImage = document.querySelector(".activeMember");
+			const crewData = data.crew;
+			let clickMember = crewNav[z].firstChild.textContent;
+			console.log(clickMember);
+			for (let i = 0; i < crewData.length; i++) {
+				if (clickMember === crewData[i].name) {
+					crewName.textContent = crewData[i].name;
+					role.textContent = crewData[i].role;
+					crewDescription.textContent = crewData[i].bio;
+					crewImage.src = crewData[i].images.webp;
+				}
+			}
+			for (let x = 0; x < crewNav.length; x++) {
+				if (crewNav[x].classList.contains("active")) {
+					crewNav[x].classList.remove("active");
+				}
+				crewNav[z].classList.add("active");
+			}
+		} catch (e) {
+			console.log("error");
+			console.log(e);
+		}
+	});
+}
+
+//pick technology
